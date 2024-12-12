9e0f4808b7f134846b5548fa15e23f2dfba8fb12
Muhammad Saheed <muhammad.saheed.iam@gmail.com>
2024-12-12 09:30:38 +0530

    Generate cert given a CSV

--- main.py
+++ main.py
@@ -68,23 +68,33 @@ def generate_cert(template_path: str, name: str, save_as: str):
         draw = ImageDraw.Draw(template)
         font_path = "./fonts/Helvetica-Bold.ttf"
 
-        max_font_size = 200
+        max_font_size = 80
         for font_size in range(max_font_size, 10, -5):
             font = ImageFont.truetype(font_path, font_size)
             text_width = font.getlength(name)
             
-            if text_width <= (template.width - 80):
-                text_x = (template.width - text_width) / 2
+            if text_width <= (template.width * 0.7):
+                text_x = 100
 
                 bbox = font.getbbox(name)
                 text_height = bbox[3] + bbox[1]
-                text_y = template.height - 100 - text_height
+                text_y = template.height / 2 - text_height - 5
 
-                draw.text((text_x, text_y), name, font=font, fill=(255, 255, 255))
+                draw.text((text_x, text_y), name, font=font, fill=(0, 0, 0))
                 break
 
         pdf_path = save_as or f'{name.lower().replace(' ', '_')}.pdf'
         template.save(pdf_path)
+
+def process_cert_csv(csv_file, output_dir):
+    if not output_dir: output_dir = 'certs'
+    os.makedirs(output_dir, exist_ok=True)
+
+    with open(csv_file, mode='r', newline='', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            generate_cert(row['template'], row['name'], f"{output_dir}/{row['name'].lower().replace(' ', '_')}.pdf")
 
 def to_title_case(image_name):
     full_name = os.path.splitext(image_name)[0].replace('_', ' ').split(' ')
@@ -95,15 +105,15 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Volunteer ID Generator")
     subparsers = parser.add_subparsers(dest='command', required=True)
 
-    cert_parser = subparsers.add_parser('id', description="Generate IDs")
-    cert_parser.add_argument('--template', type=str, help="Path to the template image")
-    cert_parser.add_argument('--name', type=str, help="Profile name")
-    cert_parser.add_argument('--profile-pic', type=str, help="Path to the profile picture")
-    cert_parser.add_argument('--output', type=str, help="Path to save the ID")
-    cert_parser.add_argument('--font-path', type=str, help="Path to a TTF font")
-    cert_parser.add_argument('--output-dir', type=str, help="Directory to save IDs when using CSV or a directory of images")
-    cert_parser.add_argument('--csv', type=str, help="CSV file with template, name, profile-pic")
-    cert_parser.add_argument('--directory', type=str, help="Directory with picture names as the person's name")
+    id_parser = subparsers.add_parser('id', description="Generate IDs")
+    id_parser.add_argument('--template', type=str, help="Path to the template image")
+    id_parser.add_argument('--name', type=str, help="Profile name")
+    id_parser.add_argument('--profile-pic', type=str, help="Path to the profile picture")
+    id_parser.add_argument('--output', type=str, help="Path to save the ID")
+    id_parser.add_argument('--font-path', type=str, help="Path to a TTF font")
+    id_parser.add_argument('--output-dir', type=str, help="Directory to save IDs when using CSV or a directory of images")
+    id_parser.add_argument('--csv', type=str, help="CSV file with template, name, profile-pic")
+    id_parser.add_argument('--directory', type=str, help="Directory with picture names as the person's name")
 
     cert_parser = subparsers.add_parser('cert', description="Generate certs")
     cert_parser.add_argument('--template', type=str, help="Path to the template image")
@@ -111,6 +121,7 @@ if __name__ == '__main__':
     cert_parser.add_argument('--output', type=str, help="Path to save the ID")
     cert_parser.add_argument('--font-path', type=str, help="Path to a TTF font")
     cert_parser.add_argument('--output-dir', type=str, help="Directory to save IDs when using CSV or a directory of images")
+    cert_parser.add_argument('--csv', type=str, help="CSV file with template, name, profile-pic")
     cert_parser.add_argument('--directory', type=str, help="Directory with picture names as the person's name")
 
     args = parser.parse_args()
@@ -134,4 +145,9 @@ if __name__ == '__main__':
         else:
             print("Error: Provide a CSV file with --csv and --output-dir, or the necessary individual arguments (--template, --name, --profile-pic), or (--template, --directory, --output-dir).")
     elif args.command == 'cert':
-        pass
+        if args.template and args.name:
+            generate_cert(args.template, args.name, args.output)
+        elif args.csv and args.output_dir:
+            process_cert_csv(args.csv, args.output_dir)
+        else:
+            print("Error: Provide the necessary individual arguments (--template, --directory, --output-dir).")
